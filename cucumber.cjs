c7c2f70260cf65cf1c343adeadff659b68a08ac2
Ian Homer <code@ianhomer.com>
2025-07-29 12:28:51 +0100

    lint: fix

--- cucumber.cjs
+++ cucumber.cjs
@@ -3,9 +3,9 @@ module.exports = {
     import: ['features/step_definitions/**/*.ts', 'features/support/**/*.ts'],
     loader: ['ts-node/esm'],
     format: [
-      'progress', 
+      'progress',
       'json:reports/cucumber_report.json',
-      'html:reports/cucumber_report.html'
+      'html:reports/cucumber_report.html',
     ],
     formatOptions: {
       snippetInterface: 'async-await',
